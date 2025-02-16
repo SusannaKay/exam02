7d4894bb91b7d662abebef82e9b9108a158392c9
Susanna kayed <kefka.sk@gmail.com>
2025-02-16 12:17:22 +0100

    atoit

---
+++ ft_atoi.c
@@ -0,0 +1,24 @@
+int ft_atoi(const char *str)
+{
+    int result;
+    int sign;
+
+    sign = 1;
+    result = 0;
+    {
+        while (*str == 32 || (*str >= 9 && *str <= 13))
+            str++;
+        if (*str == '+' || *str == '-')
+        {
+            if (*str == '-')
+                sign *= -1;
+            str++;
+        }
+        while (*str >= '0' && *str <= '9')
+        {
+            result = result * 10 + (*str - '0');
+            str++;
+        }
+    }
+    return (result * sign);
+}
