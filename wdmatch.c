c8739cf9819f0799a370bbc3504a61011b147c1d
Susanna kayed <kefka.sk@gmail.com>
2025-02-23 22:15:39 +0100

    level 2 completed

--- wdmatch.c
+++ wdmatch.c
@@ -8,4 +8,33 @@ it simply displays a \n.
 
 If the number of arguments is not 2, the program displays a \n.
 */
+#include <unistd.h>
+void putstr(char *s)
+{
+    while(*s)
+        {write(1, &(*s), 1);
+        s++;}
 
+}
+
+int main (int argc, char *argv[])
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    if (argc == 3)
+    {
+        while(argv[2][j])
+        {
+            if (argv[1][i] == argv[2][j])
+                i++;
+            j++;
+        }
+        if(!argv[1][i])
+            putstr(argv[1]);
+    }
+    write(1, "\n", 1);
+    return(0);
+}
