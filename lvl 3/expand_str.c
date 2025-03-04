4ec93a2f78e60668d6399ac7234eb0fe3f787d70
Susanna kayed <kefka.sk@gmail.com>
2025-03-04 09:12:19 +0100

    Fix: expand_string exercise

---
+++ lvl 3/expand_str.c
@@ -0,0 +1,80 @@
+/*
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and displays it with exactly three spaces
+between each word, with no spaces or tabs either at the beginning or the end,
+followed by a newline.
+
+A word is a section of string delimited either by spaces/tabs, or by the
+start/end of the string.
+
+If the number of parameters is not 1, or if there are no words, simply display
+a newline.
+
+Examples:
+
+$> ./expand_str "See? It's easy to print the same thing" | cat -e
+See?   It's   easy   to   print   the   same   thing$
+$> ./expand_str " this        time it      will     be    more complex  " | cat -e
+this   time   it   will   be   more   complex$
+$> ./expand_str "No S*** Sherlock..." "nAw S*** ShErLaWQ..." | cat -e
+$
+$> ./expand_str "" | cat -e
+$
+$>
+*/
+#include <unistd.h>
+int is_space(char c)
+{
+    if (c == 32 || c == 9)
+        return (1);
+    return (0);
+}
+void print_spaces(void)
+{
+    write(1, "   ", 3);
+}
+char *last_word_pos(char *s)
+{
+    while (*s)
+    {
+        s++;
+    }
+    s--;
+    while (*s == 32 || *s == 9)
+        s--;
+    return (s);
+}
+int main(int argc, char *argv[])
+{
+    char *last_char;
+
+    if (argc == 2)
+    {
+        if (!(*argv[1]))
+        {
+            write(1, "\n", 1);
+            return (0);
+        }
+        last_char = last_word_pos(argv[1]);
+        while (*argv[1] && last_char)
+        {
+            while (is_space(*argv[1]))
+                argv[1]++;
+            if (*argv[1] && !is_space(*argv[1]))
+            {
+                write(1, &(*argv[1]), 1);
+                argv[1]++;
+            }
+            if (is_space(*argv[1]) && argv[1] <= last_char)
+            {
+                print_spaces();
+                while (is_space(*argv[1]))
+                    argv[1]++;
+            }
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
